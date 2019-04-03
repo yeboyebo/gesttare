12c1b0ef3734878887f6e0421ece189399c657f6
Juanma Yeboyebo <juanma.yeboyebo@gmail.com>
2019-04-03 12:09:41 +0200

    companias

---
+++ model_flgesttare__gt_companias.py
@@ -0,0 +1,39 @@
+# @class_declaration interna_gt_companias #
+import importlib
+
+from YBUTILS.viewREST import helpers
+
+from models.flgesttare import models as modelos
+
+
+class interna_gt_companias(modelos.mtd_gt_companias, helpers.MixinConAcciones):
+    pass
+
+    class Meta:
+        proxy = True
+
+
+# @class_declaration gesttare_gt_companias #
+class gesttare_gt_companias(interna_gt_companias, helpers.MixinConAcciones):
+    pass
+
+    class Meta:
+        proxy = True
+
+
+# @class_declaration gt_companias #
+class gt_companias(gesttare_gt_companias, helpers.MixinConAcciones):
+    pass
+
+    class Meta:
+        proxy = True
+
+    def getIface(self=None):
+        return form.iface
+
+
+definitions = importlib.import_module("models.flgesttare.gt_companias_def")
+form = definitions.FormInternalObj()
+form._class_init()
+form.iface.ctx = form.iface
+form.iface.iface = form.iface
